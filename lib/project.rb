0318c1b36667fef47a46ffe850fb57cd3f780e9f
nathalie <nathalie.scruz@gmail.com>
2018-07-06 02:26:21 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = [ ]
+  end
+
+  def add_backer (backers)
+    @backers << backers
+    backers.backed_projects << self
+  end
+
+end
