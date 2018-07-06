973e196b0bd99d0ed1212e7a981e2d2a6815dd9a
nathalie <nathalie.scruz@gmail.com>
2018-07-06 01:12:02 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def back_project (project)
+      @project = project
+      @backed_projects = [ ]
+      @backed_projects << @project
+
+  end
+
+  
+
+end
